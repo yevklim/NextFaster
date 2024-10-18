1a93d4c70491317ce128bed721616bcd6e1f2980
Ethan Niser <ethan@niser.com>
2024-10-18 02:23:14 -0400

    fix layout spacing

--- src/app/(category-sidebar)/layout.tsx
+++ src/app/(category-sidebar)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const allCategories = artSupplies.flatMap((item) => item.categories);
   return (
     <div className="flex flex-grow font-helvetica-roman">
-      <aside className="hidden w-64 border-r border-gray-400 p-3 md:block">
+      <aside className="hidden w-48 min-w-48 border-r border-gray-400 p-3 md:block">
         <h2 className="border-b border-green-800 text-sm font-semibold text-green-900">
           Choose a Category
         </h2>
@@ -12,7 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           {allCategories.map((category) => (
             <li key={category.categoryName}>
               <a href={`/products/${category.categoryName}`}>
-                <div className="py-1 text-xs text-gray-800 hover:bg-yellow-100 hover:underline">
+                <div className="text-wrap py-1 text-xs text-gray-800 hover:bg-yellow-100 hover:underline">
                   {category.categoryName}
                 </div>
               </a>
