a15f00ffa1ccf7101159b0517145dc69607aaa75
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-17 18:53:46 -0700

    homepage clickign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
         <h2 className="text-xl font-semibold mb-4">{category.name}</h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
           {category.subcategories.map((subcategory) => (
-            <div key={subcategory.name} className="flex flex-col items-center text-center">
+            <div key={subcategory.name} className="flex flex-col items-center text-center ">
               <img
                 src={subcategory.icon}
                 alt={subcategory.name}
-                className="w-12 h-12 mb-2"
+                className="w-14 h-14 border mb-2 hover:bg-yellow-300"
                 width={48}
                 height={48}
               />
