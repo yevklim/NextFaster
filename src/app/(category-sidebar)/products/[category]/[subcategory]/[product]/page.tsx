0d9eafb1283e3f5a692e7c3d46a66f3a6bd272ff
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-18 10:42:07 -0700

    quality

--- src/app/(category-sidebar)/products/[category]/[subcategory]/[product]/page.tsx
+++ src/app/(category-sidebar)/products/[category]/[subcategory]/[product]/page.tsx
@@ -44,6 +44,7 @@ export default async function Page(props: {
             src={productData.image_url ?? "/placeholder.svg?height=64&width=64"}
             alt={`A small picture of ${productData.name}`}
             height={256}
+            quality={80}
             width={256}
             className="h-64 w-64 flex-shrink-0 border-2"
           />
