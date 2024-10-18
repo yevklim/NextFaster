42c3573cd9e446bdd616ad5e3710cac600c07b5e
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-17 21:13:45 -0700

    move to real fake data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
 import Link from "next/link";
-import { productCategories } from "./data";
+import { artSupplies } from "./data";
 
 export default function Home() {
   return (
     <main className="flex-1 p-4">
-      {productCategories.map((category) => (
-        <div key={category.name} className="mb-8">
-          <h2 className="mb-4 text-xl font-semibold">{category.name}</h2>
+      {artSupplies.map((collection) => (
+        <div key={collection.collectionName} className="mb-8">
+          <h2 className="mb-4 text-xl font-semibold">
+            {collection.collectionName}
+          </h2>
           <div className="grid grid-cols-2 gap-4 border-b-2 pb-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-            {category.subcategories.map((subcategory) => (
+            {collection.categories.map((category) => (
               <Link
-                key={subcategory.name}
+                key={category.categoryName}
                 className="flex flex-col items-center text-center"
-                href={`/products/${subcategory.name}`}
-                href="/products/subcategory"
+                href={`/products/${category.categoryName}`}
               >
                 <img
-                  src={subcategory.icon}
-                  alt={subcategory.name}
+                  src={category.icon}
+                  alt={category.categoryName}
                   className="mb-2 h-14 w-14 border hover:bg-yellow-200"
                   width={48}
                   height={48}
                 />
-                <span className="text-xs">{subcategory.name}</span>
+                <span className="text-xs">{category.categoryName}</span>
               </Link>
             ))}
           </div>
