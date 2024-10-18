0b4d7606947cb7809de7ceb7f26c3798cddece97
Arman <arman.kumaraswamy@gmail.com>
2024-10-18 02:42:20 -0400

    Fix schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -16,7 +16,7 @@ export const categories = pgTable("categories", {
 export const subcollection = pgTable("subcollections", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
-  category_id: integer("category_id")
+  category_slug: text("category_slug")
     .notNull()
     .references(() => categories.slug, { onDelete: "cascade" }),
 });
@@ -34,7 +34,7 @@ export const products = pgTable("products", {
   name: text("name").notNull(),
   description: text("description").notNull(),
   price: numeric("price").notNull(),
-  subcategory_id: integer("subcategory_id")
+  subcategory_slug: text("subcategory_slug")
     .notNull()
     .references(() => subcategories.slug, { onDelete: "cascade" }),
 });
