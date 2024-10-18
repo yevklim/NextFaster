f470026eba887fc8ce71f2608ca00cdd6c54315a
Ethan Niser <ethan@niser.com>
2024-10-18 07:15:28 -0400

    add script

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ A  replica of [McMaster-Carr](https://www.mcmaster.com/) using Next.js and AI ge
   - All mutations are done via [Server Actions](https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations)
 - Uses [Drizzle ORM](https://orm.drizzle.team/docs/overview) on top of [@vercel/postgres](https://vercel.com/docs/storage/vercel-postgres)
 - Images storied on [Vercel Blob](https://vercel.com/docs/storage/vercel-blob)
-- Used [v0](https://v0.dev) to generate all initial UIs with
+- Used [v0](https://v0.dev) to generate all initial UIs, check out some of the threads we were particularly impressed by:
+  - [v0 makes pretty impressive search dropdown without a library](https://v0.dev/chat/lFfc68X3fir?b=b_1o4tkiC9EEm&p=0)
+  - [recreating 'order' page](https://v0.dev/chat/RTBa8dXhx03?b=b_4RguNNUEhLh)
+  - [recreating 'login' page](https://v0.dev/chat/tijwMFByNX9?b=b_XnRtduKn2oe)
 - [PPR](https://vercel.com/blog/partial-prerendering-with-next-js-creating-a-new-default-rendering-model) is used to precompute the shells of pages
   - When deployed, these are served statically from the edge
   - This makes TTFB faster and speeds up CSS/fonts while origin streams
---
+++ scripts/genProducts.ts
@@ -0,0 +1,228 @@
+import { db } from "../src/db";
+import { Effect, Console } from "effect";
+import {
+  products as products_table,
+  categories,
+  subcategories as subcategories_table,
+  subcategories,
+  products,
+  subcollection,
+} from "../src/db/schema";
+import { eq, sql, lt } from "drizzle-orm";
+import OpenAI from "openai";
+import { z } from "zod";
+import slugify from "slugify";
+
+const productValidator = z.object({
+  products: z.array(
+    z.object({
+      name: z.string(),
+      description: z.string(),
+      price: z.number(),
+    }),
+  ),
+});
+
+const categoryValidator = z.object({
+  categories: z.array(
+    z.object({
+      name: z.string(),
+    }),
+  ),
+});
+
+const openai = new OpenAI();
+
+const makeProductPrompt = (categoryName: string) => `
+  You are given the name of a product category for products in an art supply store.
+  Your task is to generate 10 products. Each product has a name, description, and price.
+
+  YOU MUST OUTPUT IN ONLY JSON.
+
+  EXAMPLE:
+
+  INPUT:
+  Category Name: Painting Supplies
+
+  OUTPUT:
+  {
+    "products": [
+      {
+        "name": "Acrylic Paints (Basic and Professional Grades)",
+        "description": "High-quality, student-grade acrylic paint with smooth consistency.",
+        "price": 19.99,
+      }, 
+    ...
+  }
+
+  NOW YOUR TURN,
+
+  INPUT:
+  Category Name: ${categoryName}
+
+  OUTPUT:`;
+
+const makeCategoryPrompt = (categoryName: string) => `
+  You are given the name of a product category for products in an art supply store.
+  Your task is to generate 10 sub-categories. Each sub-category has a name.
+
+  YOU MUST OUTPUT IN ONLY JSON.
+
+  EXAMPLE:
+
+  INPUT:
+  Category Name: Sketching Pencils
+
+  OUTPUT:
+  {
+    "categories": [
+      {
+        "name": "Colored Pencils",
+      }, 
+      {
+        "name": "Charcoal Pencils",
+      }, 
+    ...
+  }
+
+  NOW YOUR TURN,
+
+  INPUT:
+  Category Name: ${categoryName}
+
+  OUTPUT:`;
+
+const main = Effect.gen(function* () {
+  // find subcollections with less than 5 subcategories
+  const subcollectionsWithLessThan5Subcategories = yield* Effect.tryPromise(
+    () =>
+      db
+        .select({
+          subcollectionId: subcollection.id,
+          subcollectionName: subcollection.name,
+          subcategoryCount: sql<number>`COUNT(${subcategories.slug})`,
+        })
+        .from(subcollection)
+        .leftJoin(
+          subcategories,
+          eq(subcollection.id, subcategories.subcollection_id),
+        )
+        .groupBy(subcollection.id, subcollection.name)
+        .having(eq(sql<number>`COUNT(${subcategories.slug})`, 0)),
+  );
+
+  console.log(
+    `found ${subcollectionsWithLessThan5Subcategories.length} subcollections with no subcategories`,
+  );
+  let counter1 = 0;
+  yield* Effect.all(
+    subcollectionsWithLessThan5Subcategories.map((coll) =>
+      Effect.gen(function* () {
+        console.log(
+          `starting ${counter1++} of ${subcollectionsWithLessThan5Subcategories.length}`,
+        );
+        console.log("starting", coll.subcollectionName);
+        const res = yield* Effect.tryPromise(() =>
+          openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: makeCategoryPrompt(coll.subcollectionName),
+              },
+            ],
+          }),
+        );
+        const json = res.choices[0].message.content;
+        if (!json) {
+          return yield* Effect.fail("no json");
+        }
+        console.log("json", json);
+        const res2 = categoryValidator.safeParse(JSON.parse(json));
+        if (!res2.success) {
+          return yield* Effect.fail("invalid json");
+        }
+        yield* Effect.all(
+          res2.data.categories
+            .map(
+              (category) =>
+                ({
+                  ...category,
+                  slug: slugify(category.name),
+                  subcollection_id: coll.subcollectionId,
+                }) as const,
+            )
+            .map((x) =>
+              Effect.tryPromise(() => db.insert(subcategories).values(x)).pipe(
+                Effect.catchAll((e) => Effect.void),
+              ),
+            ),
+        );
+      }),
+    ),
+    { concurrency: 1 },
+  );
+
+  // find subcategories withless than 5 products
+  const subcategoriesWithLessThan5Products = yield* Effect.tryPromise(() =>
+    db
+      .select({
+        subcategorySlug: subcategories.slug,
+        subcategoryName: subcategories.name,
+        productCount: sql<number>`COUNT(${products.slug})`,
+      })
+      .from(subcategories)
+      .leftJoin(products, eq(subcategories.slug, products.subcategory_slug))
+      .groupBy(subcategories.slug, subcategories.name)
+      .having(eq(sql<number>`COUNT(${products.slug})`, 0)),
+  );
+  console.log(
+    `found ${subcategoriesWithLessThan5Products.length} subcategories with no products`,
+  );
+  let counter2 = 0;
+  yield* Effect.all(
+    subcategoriesWithLessThan5Products.map((cat) =>
+      Effect.gen(function* () {
+        console.log(
+          `starting ${counter2++} of ${subcategoriesWithLessThan5Products.length}`,
+        );
+        const res = yield* Effect.tryPromise(() =>
+          openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: makeProductPrompt(cat.subcategoryName),
+              },
+            ],
+          }),
+        );
+        const json = res.choices[0].message.content;
+        if (!json) {
+          return yield* Effect.fail("no json");
+        }
+        const res2 = productValidator.safeParse(JSON.parse(json));
+        if (!res2.success) {
+          return yield* Effect.fail("invalid json");
+        }
+
+        yield* Effect.tryPromise(() =>
+          db.insert(products).values(
+            res2.data.products.map((product) => ({
+              ...product,
+              price: product.price.toString(),
+              subcategory_slug: cat.subcategorySlug,
+              slug: slugify(product.name),
+            })),
+          ),
+        );
+      }),
+    ),
+    {
+      concurrency: 5,
+    },
+  );
+});
+
+const exit = await Effect.runPromiseExit(main);
+console.log(exit.toString());
