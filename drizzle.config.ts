24d87c657d681d8d620be5bbef13b97e26bff1ad
Arman <arman.kumaraswamy@gmail.com>
2024-10-18 00:27:30 -0400

    Added drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,12 @@
+import "./drizzle/envConfig";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
   verbose: true,
   strict: true,
 });
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,45 @@
-import {
-  pgTable,
-  serial,
-  text,
-  timestamp,
-  uniqueIndex,
-} from "drizzle-orm/pg-core";
+import { integer, numeric, pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const users = pgTable(
-  "users",
-  {
-    id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull(),
-    image: text("image").notNull(),
-    createdAt: timestamp("createdAt").defaultNow().notNull(),
-  },
-  (users) => {
-    return {
-      uniqueIdx: uniqueIndex("unique_idx").on(users.email),
-    };
-  },
-);
+export const collections = pgTable("collections", {
+  id: serial("id").primaryKey(),
+  title: text("name").notNull(),
+  slug: text("slug").notNull(),
+});
+
+export const categories = pgTable("categories", {
+  id: serial("id").primaryKey(),
+  title: text("name").notNull(),
+  slug: text("slug").notNull(),
+  collection_id: integer("collection_id")
+    .notNull()
+    .references(() => collections.id, { onDelete: "cascade" }),
+});
+
+export const subcollection = pgTable("subcollections", {
+  id: serial("id").primaryKey(),
+  title: text("name").notNull(),
+  slug: text("slug").notNull(),
+  category_id: integer("category_id")
+    .notNull()
+    .references(() => categories.id, { onDelete: "cascade" }),
+});
+
+export const subcategories = pgTable("subcategories", {
+  id: serial("id").primaryKey(),
+  title: text("name").notNull(),
+  slug: text("slug").notNull(),
+  subcollection_id: integer("subcollection_id")
+    .notNull()
+    .references(() => subcollection.id, { onDelete: "cascade" }),
+});
+
+export const products = pgTable("products", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  slug: text("slug").notNull(),
+  price: numeric("price").notNull(),
+  subcategory_id: integer("subcategory_id")
+    .notNull()
+    .references(() => subcategories.id, { onDelete: "cascade" }),
+});
