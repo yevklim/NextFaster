f83ea5772706b694c3ce1d98b1dc09276881f3c8
Ethan Niser <ethan@niser.com>
2024-10-17 22:31:37 -0400

    add prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,32 @@
 import Link from "next/link";
 import { productCategories } from "./data";
 
-
 export default function Home() {
   return (
     <main className="flex-1 p-4">
-    {productCategories.map((category) => (
-      <div key={category.name} className="mb-8">
-        <h2 className="text-xl font-semibold mb-4">{category.name}</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-          {category.subcategories.map((subcategory) => (
-            <Link key={subcategory.name} className="flex flex-col items-center text-center" href="/products/subcategory">
-              <img
-                src={subcategory.icon}
-                alt={subcategory.name}
-                className="w-14 h-14 border mb-2 hover:bg-yellow-300"
-                width={48}
-                height={48}
-              />
-              <span className="text-xs">{subcategory.name}</span>
-            </Link>
-          ))}
+      {productCategories.map((category) => (
+        <div key={category.name} className="mb-8">
+          <h2 className="mb-4 text-xl font-semibold">{category.name}</h2>
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+            {category.subcategories.map((subcategory) => (
+              <Link
+                key={subcategory.name}
+                className="flex flex-col items-center text-center"
+                href="/products/subcategory"
+              >
+                <img
+                  src={subcategory.icon}
+                  alt={subcategory.name}
+                  className="mb-2 h-14 w-14 border hover:bg-yellow-300"
+                  width={48}
+                  height={48}
+                />
+                <span className="text-xs">{subcategory.name}</span>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    ))}
-  </main>
+      ))}
+    </main>
   );
 }
