d2f369b454325ae6f09f1769020af3d6a66ec6a9
Ethan Niser <ethan@niser.com>
2024-10-17 21:57:17 -0400

    add cart utils

---
+++ src/lib/cart.ts
@@ -0,0 +1,33 @@
+import { cookies } from "next/headers";
+import { z } from "zod";
+
+const cartSchema = z.array(
+  z.object({
+    id: z.string(),
+    quantity: z.number(),
+  })
+);
+
+type CartItem = {
+  id: string;
+  quantity: number;
+};
+
+export async function updateCart(newItems: CartItem[]) {
+  (await cookies()).set("cart", JSON.stringify(newItems), {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 7, // 1 week
+  });
+}
+
+export async function getCart() {
+  const cart = (await cookies()).get("cart");
+
+  if (!cart) {
+    return [];
+  }
+
+  return cartSchema.parse(JSON.parse(cart.value));
+}
