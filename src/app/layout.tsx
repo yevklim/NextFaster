229c6155b2913629a6437555cc46a5936d8081eb
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-18 16:37:30 -0700

    fix accessibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,7 @@ export default async function RootLayout({
               <Link
                 prefetch={true}
                 href="/order-history"
+                aria-label="Order History"
                 className="block text-lg text-green-800 hover:underline md:hidden"
               >
                 <MenuIcon />
