e5eb762241f31d99d326fa7a1a528b93170f734c
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-18 19:29:59 -0700

    fix ios scaling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,12 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </head>
       <body
         className={`${helvetica.variable} ${helveticaRoman.variable} ${futura.variable} flex min-h-full flex-col antialiased`}
       >
