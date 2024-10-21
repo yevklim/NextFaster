11725ee6ecceebb76c1d11871ec9a6b2b5b0de77
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-20 18:13:56 -0700

    improve search

--- src/components/search-dropdown.tsx
+++ src/components/search-dropdown.tsx
@@ -7,35 +7,31 @@ import { Search, X, Loader2 } from "lucide-react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Product } from "../db/schema";
-import { searchProducts } from "../lib/actions";
 import { Link } from "@/components/ui/link";
 import { useParams, useRouter } from "next/navigation";
+import { ProductSearchResult } from "@/app/api/search/route";
 
 type SearchResult = Product & { href: string };
 
 export function SearchDropdownComponent() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [committedSearchTerm, setCommittedSearchTerm] = useState("");
   const [filteredItems, setFilteredItems] = useState<SearchResult[]>([]);
   const [isOpen, setIsOpen] = useState(false);
   const [highlightedIndex, setHighlightedIndex] = useState(-1);
-  const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
 
+  // we don't need react query, we have react query at home
+  // react query at home:
   useEffect(() => {
     if (searchTerm.length === 0) {
       setFilteredItems([]);
-      setCommittedSearchTerm("");
     } else {
       const currentSearchTerm = searchTerm;
-      startTransition(() => {
-        searchProducts(currentSearchTerm).then((results) => {
-          if (currentSearchTerm === searchTerm) {
-            setFilteredItems(results);
-            setCommittedSearchTerm(currentSearchTerm);
-          }
-        });
+      fetch(`/api/search?q=${currentSearchTerm}`).then(async (results) => {
+        const json = await results.json();
+
+        setFilteredItems(json as ProductSearchResult);
       });
     }
   }, [searchTerm]);
@@ -84,11 +80,6 @@ export function SearchDropdownComponent() {
             onKeyDown={handleKeyDown}
             className="font-sans font-medium sm:w-[300px] md:w-[375px]"
           />
-          {isPending ? (
-            <Loader2 className="absolute right-2 top-2.5 h-4 w-4 animate-spin text-muted-foreground" />
-          ) : (
-            <Search className="absolute right-2 top-2.5 h-4 w-4 text-muted-foreground" />
-          )}
           <X
             className={cn(
               "absolute right-7 top-2 h-5 w-5 text-muted-foreground",
@@ -102,7 +93,7 @@ export function SearchDropdownComponent() {
             }}
           />
         </div>
-        {((isOpen && committedSearchTerm === searchTerm) || false) && (
+        {isOpen && (
           <div className="absolute z-10 w-full border border-gray-200 bg-white shadow-lg">
             <ScrollArea className="h-[300px]">
               {filteredItems.length > 0 ? (
