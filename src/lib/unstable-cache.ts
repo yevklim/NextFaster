cd4051fa150ede39dc8ff8ad9cc97d60653a78e8
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-20 23:11:03 -0700

    clarity

--- src/lib/unstable-cache.ts
+++ src/lib/unstable-cache.ts
@@ -3,7 +3,7 @@ import {cache} from "react";
 
 type Callback<T> = (...args: any[]) => Promise<T>;
 
-
+// next_unstable_cache doesn't handle deduplication, so we wrap it in React's cache
 export const unstable_cache = <T>(callback: Callback<T>, key: string[], options: {revalidate: number}) => cache(
     next_unstable_cache(callback, key, options)
 )
